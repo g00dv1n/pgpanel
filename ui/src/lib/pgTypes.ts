2f7145b248da22cd3878da601df85979c6091a5e
Alex Brik <g00dv1n.private@gmail.com>
2024-11-20 22:01:48 +0200

    set up pg input types

--- ui/src/lib/pgTypes.ts
+++ ui/src/lib/pgTypes.ts
@@ -111,13 +111,79 @@ export enum PgTypeOID {
   Int8multirangeArrayOID = 6157,
 }
 
+export type DefaultInputType = "checkbox" | "input" | "textarea" | "datepicker";
+export type DefaultInputTypeLookup = {
+  type: DefaultInputType;
+  isArray?: boolean;
+};
+
+export const MainPgTypesOidMap: Record<
+  number,
+  { type: DefaultInputType; isArray?: boolean }
+> = {
+  // Boolean
+  [PgTypeOID.BoolOID]: { type: "checkbox" },
+
+  // Numeric
+  [PgTypeOID.Int2OID]: { type: "input" },
+  [PgTypeOID.Int4OID]: { type: "input" },
+  [PgTypeOID.Int8OID]: { type: "input" },
+  [PgTypeOID.Float4OID]: { type: "input" },
+  [PgTypeOID.Float8OID]: { type: "input" },
+  [PgTypeOID.NumericOID]: { type: "input" },
+
+  // Textual
+  [PgTypeOID.TextOID]: { type: "textarea" },
+  [PgTypeOID.VarcharOID]: { type: "textarea" },
+  [PgTypeOID.BPCharOID]: { type: "textarea" },
+  [PgTypeOID.NameOID]: { type: "textarea" },
+
+  // Dates and Times
+  [PgTypeOID.DateOID]: { type: "datepicker" },
+  [PgTypeOID.TimestampOID]: { type: "datepicker" },
+  [PgTypeOID.TimestamptzOID]: { type: "datepicker" },
+  [PgTypeOID.TimeOID]: { type: "datepicker" },
+  [PgTypeOID.TimetzOID]: { type: "datepicker" },
+  [PgTypeOID.IntervalOID]: { type: "textarea" },
+
+  // JSON
+  [PgTypeOID.JSONOID]: { type: "textarea" },
+  [PgTypeOID.JSONBOID]: { type: "textarea" },
+  [PgTypeOID.JSONPathOID]: { type: "textarea" },
+
+  // Arrays
+  [PgTypeOID.TextArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.VarcharArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.Int2ArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.Int4ArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.Int8ArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.Float4ArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.Float8ArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.DateArrayOID]: { type: "datepicker", isArray: true },
+  [PgTypeOID.TimestampArrayOID]: { type: "datepicker", isArray: true },
+  [PgTypeOID.TimestamptzArrayOID]: { type: "datepicker", isArray: true },
+  [PgTypeOID.JSONBArrayOID]: { type: "textarea", isArray: true },
+  [PgTypeOID.InetArrayOID]: { type: "input", isArray: true },
+  [PgTypeOID.UUIDArrayOID]: { type: "input", isArray: true },
+};
+
 export interface PgColumn {
   name: string;
   OID: PgTypeOID;
   regType: string;
   udtName: string;
-  isNullable: string;
+  isNullable: "YES" | "NO";
   default?: string | null;
+}
+
+export function getColumnDefaultInputType(
+  column: PgColumn
+): DefaultInputTypeLookup {
+  if (MainPgTypesOidMap[column.OID]) {
+    return MainPgTypesOidMap[column.OID];
+  }
+
+  return { type: "textarea" };
 }
 
 export interface PgTable {
