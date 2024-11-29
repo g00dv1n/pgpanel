6e751cd35721664a6f1b090c08c98bc5e8a05ab1
Alex Brik <g00dv1n.private@gmail.com>
2024-11-29 16:08:28 +0200

    move crud param parsing to api handlers file

--- core/api_data.go
+++ core/api_data.go
@@ -3,13 +3,111 @@ package core
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/g00dv1n/pgpanel/db"
 )
 
+// ---------------- Query Keys For parsing Filter, Pagination and Sorting -----------------
+const (
+	TextFiltersQK = "textFilters"
+	FiltersQK     = "filters"
+	FiltersArgsQK = "filtersArgs"
+	OffsetQK      = "offset"
+	LimitQK       = "limit"
+	SortQK        = "sort"
+
+	QueryArgsDelimiter = "|"
+)
+
+// ---------------------- Parse Filters -------------------------------
+func ParseFiltersFromQuery(q url.Values) db.Filters {
+	textFilters := q.Get(TextFiltersQK)
+
+	if len(textFilters) > 0 {
+		return db.TextSearchFilters{Text: textFilters}
+	}
+
+	filters := q.Get(FiltersQK)
+	rawArgs := q.Get(FiltersArgsQK)
+
+	return ParseSQLFilters(filters, rawArgs)
+}
+
+func ParseSQLFilters(statement string, rawArgs string) db.SQLFilters {
+	if len(rawArgs) == 0 {
+		return db.SQLFilters{Statement: statement, Args: nil}
+	}
+
+	parsedArgs := strings.Split(rawArgs, QueryArgsDelimiter)
+	args := make([]any, len(parsedArgs), len(parsedArgs))
+
+	for i, v := range parsedArgs {
+		args[i] = v
+	}
+
+	return db.SQLFilters{Statement: statement, Args: args}
+}
+
+// ---------------------- Parse Pagination -------------------------------
+func ParsePaginationFromQuery(q url.Values) db.Pagination {
+	offsetStr := q.Get(OffsetQK)
+	limitStr := q.Get(LimitQK)
+
+	offset, offsetErr := strconv.Atoi(offsetStr)
+	if offsetErr != nil || offset < 0 {
+		offset = 0
+	}
+
+	limit, limitErr := strconv.Atoi(limitStr)
+	if limitErr != nil || limit < 1 {
+		limit = db.DefaultPaginationLimit
+	}
+
+	return db.Pagination{Offset: offset, Limit: limit}
+}
+
+// ---------------------- Parse Sorting -------------------------------
+func ParseSortingFromQuery(q url.Values) db.Sorting {
+	sort := q.Get(SortQK)
+
+	if len(sort) == 0 {
+		return nil
+	}
+
+	fieldsRaw := strings.Split(sort, QueryArgsDelimiter)
+	fields := make([]db.SortingField, 0, len(fieldsRaw))
+
+	for _, fr := range fieldsRaw {
+		if len(fr) == 0 {
+			continue
+		}
+
+		if strings.HasPrefix(fr, "-") {
+			fields = append(fields, db.SortingField{Name: strings.TrimPrefix(fr, "-"), Order: "DESC"})
+		} else {
+			fields = append(fields, db.SortingField{Name: fr, Order: "ASC"})
+		}
+	}
+
+	return fields
+}
+
+// ---------------------- Parse GetRowsParams  -------------------------------
+func ParseGetRowsParamsFromQuery(q url.Values) *db.GetRowsParams {
+	return &db.GetRowsParams{
+		Filters:    ParseFiltersFromQuery(q),
+		Pagination: ParsePaginationFromQuery(q),
+		Sorting:    ParseSortingFromQuery(q),
+	}
+}
+
+// ---------------------- Data Handlers -------------------------------
 func (app *App) getRowsHandler(w http.ResponseWriter, r *http.Request) error {
 	tableName := r.PathValue("table")
-	params := db.ParseGetRowsParamsFromQuery(r.URL.Query())
+	params := ParseGetRowsParamsFromQuery(r.URL.Query())
 
 	rows, err := app.CRUD.GetRows(tableName, params)
 
@@ -39,7 +137,7 @@ func (app *App) insertRowHandler(w http.ResponseWriter, r *http.Request) error {
 
 func (app *App) updateRowsHandler(w http.ResponseWriter, r *http.Request) error {
 	tableName := r.PathValue("table")
-	filters := db.ParseFiltersFromQuery(r.URL.Query())
+	filters := ParseFiltersFromQuery(r.URL.Query())
 
 	var row db.RawRow
 	if err := json.NewDecoder(r.Body).Decode(&row); err != nil {
@@ -57,7 +155,7 @@ func (app *App) updateRowsHandler(w http.ResponseWriter, r *http.Request) error 
 
 func (app *App) deleteRowsHandler(w http.ResponseWriter, r *http.Request) error {
 	tableName := r.PathValue("table")
-	filters := db.ParseFiltersFromQuery(r.URL.Query())
+	filters := ParseFiltersFromQuery(r.URL.Query())
 
 	rows, err := app.CRUD.DeleteRows(tableName, filters)
 
--- db/crud_params.go
+++ db/crud_params.go
@@ -2,23 +2,13 @@ package db
 
 import (
 	"fmt"
-	"net/url"
-	"strconv"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 const (
-	TextFiltersQK = "textFilters"
-	FiltersQK     = "filters"
-	FiltersArgsQK = "filtersArgs"
-	OffsetQK      = "offset"
-	LimitQK       = "limit"
-	SortQK        = "sort"
-
-	QueryArgsDelimiter = "|"
-	DefaultRowsLimit   = 50
+	DefaultPaginationLimit = 50
 )
 
 // ---------------------- General Filters Interface -------------------------------
@@ -26,38 +16,10 @@ type Filters interface {
 	ToSQL(table *Table) (string, []any)
 }
 
-func ParseFiltersFromQuery(q url.Values) Filters {
-	textFilters := q.Get(TextFiltersQK)
-
-	if len(textFilters) > 0 {
-		return TextSearchFilters{Text: textFilters}
-	}
-
-	filters := q.Get(FiltersQK)
-	rawArgs := q.Get(FiltersArgsQK)
-
-	return ParseSQLFilters(filters, rawArgs)
-}
-
 // ---------------------- SQLFilters -------------------------------
 type SQLFilters struct {
 	Statement string
 	Args      []any
-}
-
-func ParseSQLFilters(statement string, rawArgs string) SQLFilters {
-	if len(rawArgs) == 0 {
-		return SQLFilters{Statement: statement, Args: nil}
-	}
-
-	parsedArgs := strings.Split(rawArgs, QueryArgsDelimiter)
-	args := make([]any, len(parsedArgs), len(parsedArgs))
-
-	for i, v := range parsedArgs {
-		args[i] = v
-	}
-
-	return SQLFilters{Statement: statement, Args: args}
 }
 
 func (f SQLFilters) ToSQL(t *Table) (string, []any) {
@@ -98,54 +60,12 @@ type Pagination struct {
 	Offset int
 }
 
-func ParsePaginationFromQuery(q url.Values) Pagination {
-	offsetStr := q.Get(OffsetQK)
-	limitStr := q.Get(LimitQK)
-
-	offset, offsetErr := strconv.Atoi(offsetStr)
-	if offsetErr != nil || offset < 0 {
-		offset = 0
-	}
-
-	limit, limitErr := strconv.Atoi(limitStr)
-	if limitErr != nil || limit < 1 {
-		limit = DefaultRowsLimit
-	}
-
-	return Pagination{Offset: offset, Limit: limit}
-}
-
 // ---------------------- Sorting -------------------------------
 type SortingField struct {
 	Name  string
 	Order string
 }
 type Sorting []SortingField
-
-func ParseSortingFromQuery(q url.Values) Sorting {
-	sort := q.Get(SortQK)
-
-	if len(sort) == 0 {
-		return nil
-	}
-
-	fieldsRaw := strings.Split(sort, QueryArgsDelimiter)
-	fields := make([]SortingField, 0, len(fieldsRaw))
-
-	for _, fr := range fieldsRaw {
-		if len(fr) == 0 {
-			continue
-		}
-
-		if strings.HasPrefix(fr, "-") {
-			fields = append(fields, SortingField{Name: strings.TrimPrefix(fr, "-"), Order: "DESC"})
-		} else {
-			fields = append(fields, SortingField{Name: fr, Order: "ASC"})
-		}
-	}
-
-	return fields
-}
 
 func (s Sorting) ToSQL() string {
 	if len(s) == 0 {
@@ -174,16 +94,8 @@ type GetRowsParams struct {
 
 func DefaultGetRowsParams() *GetRowsParams {
 	return &GetRowsParams{
-		Pagination: Pagination{Limit: DefaultRowsLimit, Offset: 0},
+		Pagination: Pagination{Limit: DefaultPaginationLimit, Offset: 0},
 		Filters:    SQLFilters{},
 		Sorting:    Sorting{},
-	}
-}
-
-func ParseGetRowsParamsFromQuery(q url.Values) *GetRowsParams {
-	return &GetRowsParams{
-		Filters:    ParseFiltersFromQuery(q),
-		Pagination: ParsePaginationFromQuery(q),
-		Sorting:    ParseSortingFromQuery(q),
 	}
 }
