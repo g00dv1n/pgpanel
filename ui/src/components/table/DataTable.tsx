e8faaf3e41a05b7804fe933180c2973f6fa33f80
Alex Brik <g00dv1n.private@gmail.com>
2024-12-06 16:56:56 +0200

    blur loading overlay

--- ui/src/components/table/DataTable.tsx
+++ ui/src/components/table/DataTable.tsx
@@ -123,9 +123,9 @@ export function DataTable({
 
 function LoadingOverlay() {
   return (
-    <div className="absolute inset-0 bg-black/5 z-50 flex justify-center pt-2">
-      <div className="bg-white/50 px-4 py-2 rounded-full shadow-sm flex items-center h-fit">
-        <Loader2 className="h-6 w-6 animate-spin text-primary/70" />
+    <div className="absolute inset-0 bg-white/10 backdrop-blur-[1px] z-50 flex justify-center pt-2">
+      <div className="bg-white/30 px-4 py-4 rounded-full shadow-sm flex items-center h-fit">
+        <Loader2 className="h-6 w-6 animate-spin text-primary/50" />
       </div>
     </div>
   );
