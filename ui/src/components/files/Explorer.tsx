70b942bb1187c20e67f67b65ef79840b3263bec4
Alex Brik <g00dv1n.private@gmail.com>
2025-01-24 19:06:16 +0200

    smaller files images

--- ui/src/components/files/Explorer.tsx
+++ ui/src/components/files/Explorer.tsx
@@ -1,5 +1,6 @@
 import { StorageFileInfo } from "@/api/files";
 import { Checkbox } from "@/components/ui/checkbox";
+import { File } from "lucide-react";
 
 interface ExplorerProps {
   list: StorageFileInfo[];
@@ -8,26 +9,34 @@ interface ExplorerProps {
 }
 
 export function Explorer({ list, selected = [], onSelect }: ExplorerProps) {
+  const files = list.filter((fi) => !fi.isDir);
+
   return (
-    <div className="w-full flex gap-4 flex-wrap">
-      {list.map((info) => {
+    <div className="w-full flex gap-3 flex-wrap">
+      {files.map((info) => {
         const isSelected = selected.some((sf) => sf.name === info.name);
 
         return (
           <div
             key={info.name}
             className={`
-              flex flex-col gap-1 items-center w-60 p-1 border-2 rounded relative
+              flex flex-col gap-1 items-center w-48 p-1 border-2 rounded relative
               ${isSelected ? "border-blue-500" : ""}
               cursor-pointer hover:opacity-90 transition-all
             `}
           >
             <Checkbox
               checked={isSelected}
-              className="absolute left-2 top-2"
+              className="absolute left-2 top-1"
               onCheckedChange={(c) => onSelect && onSelect(info, c === true)}
             />
-            <img className="size-60 object-cover" src={info.internalUrl} />
+
+            {info.isImage ? (
+              <img className="size-32 object-cover" src={info.internalUrl} />
+            ) : (
+              <File className="size-32" />
+            )}
+
             <div className="w-full text-center break-words">{info.name}</div>
           </div>
         );
