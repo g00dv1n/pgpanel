0c4eb7f3dec78ed90d5c71b9b0fce4539750a3d6
Alex Brik <g00dv1n.private@gmail.com>
2026-02-02 16:09:05 +0200

    simplify eslint conf

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,27 +1,28 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
-  { ignores: ["dist"] },
+export default defineConfig([
+  globalIgnores(["dist"]),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-expressions": "off",
+      "react-refresh/only-export-components": "off",
     },
   },
-);
+]);
